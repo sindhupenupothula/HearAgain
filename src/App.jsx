f9ee57eb49aa62643aa24c0941fcd92d682786ba
sindhupenupothula <sindhupenupothula2001@gmail.com>
2026-09-25 02:22:56 -0700

    feat: blank background

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return <div style={styles.container} />
+}
+
+const styles = {
+  container: {
+    backgroundColor: '#FFF8E7',
+    minHeight: '100vh',
+    width: '100%',
+    margin: 0,
+    padding: 0,
+  },
+}
+
+export default App
